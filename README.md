503b047338ca7e0e541b7b3184f20f29c9852cef
davidliu <david-khala@hotmail.com>
2021-02-02 12:15:53 +0800

    add tableSpace clause

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # node-oracledb-wheel
 node-oracledb enhancement tool
+
+
+## Notes
+- Oracle comes with the following default tablespaces: `SYSTEM`, `SYSAUX`, `USERS`, `UNDOTBS1`, and `TEMP`.
--- data-safe/setup.js
+++ data-safe/setup.js
@@ -1,3 +1,6 @@
+/**
+ * Reference: https://docs.oracle.com/en-us/iaas/data-safe/doc/register-db-systems-that-have-public-ip-addresses.html#UDSCS-GUID-06A42C4E-1132-4E80-8F83-E553FB3A8AED
+ */
 class DataSafeSetup {
     /**
      *
@@ -7,8 +10,17 @@ class DataSafeSetup {
         this.connectionManager = connectionManager
     }
 
-    async createServiceAccount(DATASAFE_ADMIN, password) {
-        const SQL = `CREATE USER ${DATASAFE_ADMIN} identified by ${password};
+    /**
+     * @param {string} DATASAFE_ADMIN
+     * @param {string} password
+     * @param {string} defaultTablespace
+     */
+    async createServiceAccount(DATASAFE_ADMIN, password, defaultTablespace = 'USERS') {
+        if (['SYSTEM', 'SYSAUX'].includes(defaultTablespace.toUpperCase())) {
+            throw Error(`Do not use 'SYSTEM' or 'SYSAUX' as the default tablespace. You cannot mask data if you use these tableSpaces.`)
+        }
+
+        const SQL = `CREATE USER ${DATASAFE_ADMIN} identified by ${password} DEFAULT TABLESPACE "${defaultTablespace}" TEMPORARY TABLESPACE "TEMP";
             GRANT CONNECT,RESOURCE,ASSESSMENT,AUDIT_COLLECTION,DATA_DISCOVERY,MASKING,AUDIT_SETTING TO ${DATASAFE_ADMIN};`
         await this.connectionManager.execute(SQL)
     }
